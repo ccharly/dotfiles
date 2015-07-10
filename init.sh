a7724bf2d4b37a762a70d7d015bdf5dbe7d8872c
Charly Chevalier <charly.chevalier@numscale.com>
2015-07-10 11:38:30 +0200

    update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 PWD=$( pwd )
-HOME=~
 
 make_symlink() {
 	ln -sf $PWD/$1 $HOME/$2
@@ -16,8 +15,20 @@ make_home_symlink() {
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 
 # Get powerline-fonts
-git clone https://github.com/Lokaltog/powerline-fonts.git /tmp/powerline-fonts && bash /tmp/powerline-fonts/install.sh
+if [ ! -e /tmp/powerline-fonts ]; then
+    git clone https://github.com/Lokaltog/powerline-fonts.git /tmp/powerline-fonts
+fi
+bash /tmp/powerline-fonts/install.sh # Does not work with `sh`..
+
+# Move existing .vim/ to .vim.$$.bk if existing
+if [ -e "$HOME/.vim" ]; then
+	echo "Backing up your old .vim directory"
+  	mv $HOME/.vim $HOME/.vim.$$.bk
+fi
 
 make_symlink "dot.vim" ".vim"
 make_home_symlink "dot.vimrc" ".vim/vimrc" ".vimrc"
 make_home_symlink "dot.vimrc.plugins" ".vim/vimrc.plugins" ".vimrc.plugins"
+
+# Everything is done now!
+echo "Done!"
