11cf7678f23c94dcbeda05c33a9abcbd821ebedd
ccharly <charlyy.chevalier@gmail.com>
2016-04-10 19:52:04 +0200

    Update

--- README.md
+++ README.md
@@ -52,3 +52,41 @@ FlagsForFile = ycm.FlagsForFile
 Keep in mind that you have to provide a `FlagsForFile` function to `ycm`.
 
 That's it :) Give it a shot, it's pretty handy to configure.
+
+## Install vim from sources with python support for YCM
+
+You first need to install `libpython-dev` to enable python support.
+
+```
+sudo apt-get install libpython-dev
+```
+Make sure every vim has been removed as you want to use the new one to get everything working well
+with YCM!
+
+```
+sudo apt-get remove vim-tiny vim-common vim-gui-common vim vim-runtime gvim
+```
+
+```
+cd ~
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ \
+            --enable-perlinterp \
+            --enable-luainterp \
+            --enable-gui=gtk2 \
+			--enable-cscope
+sudo make install
+```
+
+## Install zsh
+
+You MUST install `zsh` first to install `oh-my-zsh`
+
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+```
--- init.sh
+++ init.sh
@@ -11,8 +11,30 @@ make_home_symlink() {
 	ln -sf $HOME/$2 $HOME/$3
 }
 
+has() {
+    if ! which $1; then
+      echo "error: install '$1' first"
+      exit 1
+    fi
+}
+
+# Move to init.sh directory
+cd $( dirname $0 )
+
+# Check required stuff
+has curl
+has ctags
+
+# Move existing .vim/ to .vim.$$.bk if existing
+if [ -e $HOME/.vim ] && [ ! -e $HOME/.vim/vimrc.plugins ]; then
+	echo "Backing up your old .vim directory"
+  	mv $HOME/.vim $HOME/.vim.$$.bk
+fi
+
 # Get NeoBundle for vim
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
+    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+fi
 
 # Get powerline-fonts
 if [ ! -e /tmp/powerline-fonts ]; then
@@ -20,13 +42,7 @@ if [ ! -e /tmp/powerline-fonts ]; then
 fi
 bash /tmp/powerline-fonts/install.sh # Does not work with `sh`..
 
-# Move existing .vim/ to .vim.$$.bk if existing
-if [ -e "$HOME/.vim" ]; then
-	echo "Backing up your old .vim directory"
-  	mv $HOME/.vim $HOME/.vim.$$.bk
-fi
-
-make_symlink "dot.vim" ".vim"
+make_symlink      "dot.vim" ".vim"
 make_home_symlink "dot.vimrc" ".vim/vimrc" ".vimrc"
 make_home_symlink "dot.vimrc.plugins" ".vim/vimrc.plugins" ".vimrc.plugins"
 
