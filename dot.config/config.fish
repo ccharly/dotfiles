e9b0bfa322501a83579c84051b832589c09a0444
Charly Chevalier <charlyy.chevalier@gmail.com>
2019-06-18 21:58:40 +0200

    Better fish setup

--- dot.config/config.fish
+++ dot.config/config.fish
@@ -6,3 +6,9 @@ set -x PATH $HOME/.rbenv/shims:$PATH
 
 ## Get fzf keybinds
 . $OMF_PATH/pkg/fzf/conf.d/fzf_key_bindings.fish
+
+## Better `ls` colors
+set -Ux LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
+
+## Set US lang
+set -x LANG en_US.UTF-8
